787052b43acfeb385be35a0b1889362ab2d375a9
Anaïs Lorteau <anaislor@orange.fr>
2019-10-21 12:28:51 +0200

    reorganisation du dossier pour une meilleur lecture

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cat Mash | Solution
+
+## Introduction
+
+Ce repo est une solution pour le challenge Cat Mash de l'Atelier.
+
+Ce Cat Mash a pour objectif de déterminer quel est le plus beau chat.
+Pour cela, la logique repose sur un tournoi à élimination directe.
+Vous pouvez ensuite accéder à la liste de tous les chats en compétition afin de découvir ceux les plus appréciés par les utilisateurs.
+
+## Commandes
+
+Pour démarer le projet la première fois, clonez le projet et lancez:
+
+```sh
+npm install #pour installer tous les packages
+npm run dev:server
+npm run dev:client
+```
+
+## Version déployée
+
+[Lien](https://catmash-anais.herokuapp.com/) pour accéder au site.
---
+++ starter-code/client/build/precache-manifest.8d8f37d10ea146e36a02d6f3880bfec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7337f8b2957e74896aefb37d291573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "379c7d0a3a940eda885c",
+    "url": "/static/css/main.c893a97c.chunk.css"
+  },
+  {
+    "revision": "217e3125461d15e21dbf",
+    "url": "/static/js/2.b2182816.chunk.js"
+  },
+  {
+    "revision": "379c7d0a3a940eda885c",
+    "url": "/static/js/main.b646c927.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d56970a932fdbe8aac71067063dac14",
+    "url": "/static/media/logo.9d56970a.svg"
+  }
+]);
